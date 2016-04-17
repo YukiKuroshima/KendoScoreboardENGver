a311b2dbf8baaf0e07bb2896caf2fb7d99f9b1bb
Yuuki K <yuuki3327@hotmail.com>
2016-04-16 17:14:20 -0700

    Initial Commit

---
+++ common.js
@@ -0,0 +1,312 @@
+var idList = ['m1', 'm2', 'm3', 'm4', 'm5']; //array for id
+var EneidList = ['Em1', 'Em2', 'Em3', 'Em4', 'Em5']; //array for id
+var colList = ["col1", "col2", "col3", "col4", "col5"];
+var n = 0; //counter for id
+var score = 0;
+var eneScore = 0;
+var winCount = 0;
+var eWinCount = 0;
+var addedScoreList = [];
+
+highLightCurrentColumn(); //highlight the first column
+
+function isTotalLessThanThree() { // if less than or equalthree return true
+    "use strict";
+    var result = true;
+    var myLength = 0;
+    var enLength = 0;
+    for(var i = 0; i < document.getElementById(idList[n]).innerHTML.length; i++){
+//console.log(document.getElementById(idList[n]).innerHTML.substr(i,i+1));
+        if(document.getElementById(idList[n]).innerHTML.substr(i,i+1)!='▲'){
+            myLength++;
+        }
+    }
+    for(var i = 0; i < document.getElementById(EneidList[n]).innerHTML.length; i++){
+        if(document.getElementById(EneidList[n]).innerHTML.substr(i,i+1)!='▲'){
+            enLength++;
+        }
+    }
+
+    if (myLength + enLength >= 3) {
+        result = false;
+    }
+    console.log(myLength + "  " + enLength);
+    return result;
+}
+
+function isThisCellFull(listID) { // return true if this cell has two or more char( means full)
+    "use strict";
+    var result = true;
+    var len = 0;
+    for(var i = 0; i < document.getElementById(listID).innerHTML.length; i++){
+//console.log(document.getElementById(listID).innerHTML.substr(i,i+1));
+        if(document.getElementById(listID).innerHTML.substr(i,i+1)!='▲'){
+            len++;
+        }
+    }
+    if(len < 2) result = false;
+    return result;
+}
+
+function getWinnerNum() {
+  "use strict";
+  score = 0;
+  eneScore =0;
+  winCount = 0;
+  eWinCount = 0;
+  var tempScore = 0;
+  var tempEneScore = 0;
+
+  for(var i = 0; i < n + 1; i++){
+      for(var j = 0; j < document.getElementById(idList[i]).innerHTML.length; j++){
+//console.log(document.getElementById(idList[i]).innerHTML.substr(j,j+1));
+          if(document.getElementById(idList[i]).innerHTML.substr(j,j+1)!='▲'){
+              score++;
+              tempScore++;
+          }
+      }
+      for(var j = 0; j < document.getElementById(EneidList[i]).innerHTML.length; j++){
+          if(document.getElementById(EneidList[i]).innerHTML.substr(j,j+1)!='▲'){
+              eneScore++;
+              tempEneScore++;
+          }
+      }
+      if(tempScore > tempEneScore){winCount++;}
+      else if(tempScore < tempEneScore){eWinCount++;}
+      tempScore = 0;
+      tempEneScore = 0;
+  }
+}
+
+function upDateScore() {
+  "use strict";
+  document.getElementById("mScore").innerHTML = score;
+  document.getElementById("eScore").innerHTML = eneScore;
+  document.getElementById("mWinNum").innerHTML = winCount;
+  document.getElementById("eWinNum").innerHTML = eWinCount;
+}
+
+function highLightCurrentColumn() {
+    "use strict";
+  //color current pos
+  var temp = document.getElementsByClassName(colList[n]);
+  if(temp[0].style.backgroundColor !== "#81C3A5"){
+    for (var i = 0; i < temp.length; i++) {
+      temp[i].style.backgroundColor = "#81C3A5";
+    }
+  }
+
+  if(n > 0 && n < 5){ //means pos is not at 0 (1 to 4)
+    var temp2 = document.getElementsByClassName(colList[n - 1]); // check before
+    if(temp2[0].style.backgroundColor != "#e0e0e0"){
+      for (var i = 0; i < temp2.length; i++) {
+        temp2[i].style.backgroundColor = "#e0e0e0";
+      }
+    }
+  }
+  if(-1 < n && n < 4){ // means n is 0 to 3(one before last element)
+    var temp3 = document.getElementsByClassName(colList[n + 1]); // check after
+    if(temp3[0].style.backgroundColor != "#e0e0e0"){
+      for (var i = 0; i < temp3.length; i++) {
+        temp3[i].style.backgroundColor = "#e0e0e0";
+      }
+    }
+  }
+}
+
+function writeWinner() {
+    "use strict";
+
+    if(winCount > eWinCount) {
+        document.getElementById("mWin").innerHTML = "Victory";
+        document.getElementById("eWin").innerHTML = "Defeat";
+    }
+    else if(winCount < eWinCount){
+        document.getElementById("eWin").innerHTML = "Victory";
+        document.getElementById("mWin").innerHTML = "Defeat";
+    }
+    else { //tie with num of win person
+        if(score > eneScore) {
+            document.getElementById("mWin").innerHTML = "Victory";
+            document.getElementById("eWin").innerHTML = "Defeat";
+        }
+        else if(score < eneScore){
+            document.getElementById("eWin").innerHTML = "Victory";
+            document.getElementById("mWin").innerHTML = "Defeat";
+        }
+        else {
+            document.getElementById("mWin").innerHTML = "Tie";
+            document.getElementById("eWin").innerHTML = "Tie";
+        }
+    }
+}
+
+function getNumScoreCurretMatch() {
+    "use strict";
+  var result;
+  result = document.getElementById(idList[n]).innerHTML.length
+    + document.getElementById(EneidList[n]).innerHTML.length;
+  return result;
+}
+
+function undo(){
+  "use strict";
+
+  if(n <= 0 && getNumScoreCurretMatch() === 0){ // n is smaller or = 0 and no char at current pos
+    alert("No more Undo allowed");
+  }
+  else if (getNumScoreCurretMatch() > 0){ // check if this pos has more than one char
+    var tempPos = addedScoreList[addedScoreList.length - 1]; //m1 ... Em1
+    var tempHTML = document.getElementById(tempPos).innerHTML;
+    document.getElementById(tempPos).innerHTML = tempHTML.substr(0, tempHTML.length-1);
+    addedScoreList.pop();
+  }
+  else {  //no more char in current Match
+    n--;
+  }
+}
+
+//my side
+document.getElementById("men").addEventListener('click', function () {
+    "use strict";
+    if (!isThisCellFull(idList[n]) && isTotalLessThanThree()) {
+        document.getElementById(idList[n]).innerHTML += 'M';
+        addedScoreList.push(idList[n]);
+        upDateScore();
+    } else {
+        alert("Click Next Button");
+    }
+}, false);
+
+document.getElementById("ko").addEventListener('click', function () {
+    "use strict";
+    if (!isThisCellFull(idList[n]) && isTotalLessThanThree()) {
+        document.getElementById(idList[n]).innerHTML += 'K';
+        addedScoreList.push(idList[n]);
+        upDateScore();
+    } else {alert("Click Next Button"); }
+}, false);
+
+document.getElementById("do").addEventListener('click', function () {
+    "use strict";
+    if (!isThisCellFull(idList[n]) && isTotalLessThanThree()) {
+        document.getElementById(idList[n]).innerHTML += 'D';
+        addedScoreList.push(idList[n]);
+        upDateScore();
+    } else {alert("Click Next Button"); }
+}, false);
+
+document.getElementById("tu").addEventListener('click', function () {
+    "use strict";
+    if (!isThisCellFull(idList[n]) && isTotalLessThanThree()) {
+        document.getElementById(idList[n]).innerHTML += 'T';
+        addedScoreList.push(idList[n]);
+        upDateScore();
+    } else {alert("Click Next Button"); }
+}, false);
+
+document.getElementById("ha").addEventListener('click', function () {
+    "use strict";
+    if (!isThisCellFull(idList[n]) && isTotalLessThanThree()) {
+        var FoundH = false;
+        var temp = document.getElementById(idList[n]).innerHTML;
+        for(var i = 0; i < document.getElementById(idList[n]).innerHTML.length; i++){
+//console.log(document.getElementById(idList[n]).innerHTML.substr(i,i+1));
+
+            if(document.getElementById(idList[n]).innerHTML.substr(i,i+1) === '▲'){
+                document.getElementById(idList[n]).innerHTML = temp.slice(0, i) + temp.slice(i + 1);
+//console.log("TEMP" + temp.slice(0, i) + temp.slice(i));
+                document.getElementById(idList[n]).innerHTML += 'H';
+                FoundH = true;
+            }
+        }
+        if(!FoundH){document.getElementById(idList[n]).innerHTML += '▲';}
+        addedScoreList.push(idList[n]);
+        upDateScore();
+    } else {alert("Click Next Button"); }
+}, false);
+
+//Opponents side
+document.getElementById("Enemen").addEventListener('click', function () {
+    "use strict";
+    if (!isThisCellFull(EneidList[n]) && isTotalLessThanThree()) {
+        document.getElementById(EneidList[n]).innerHTML += 'M';
+        addedScoreList.push(EneidList[n]);
+        upDateScore();
+    } else {alert("Click Next Button"); }
+}, false);
+
+document.getElementById("Eneko").addEventListener('click', function () {
+    "use strict";
+    if (!isThisCellFull(idList[n]) && isTotalLessThanThree()) {
+        document.getElementById(EneidList[n]).innerHTML += 'K';
+        addedScoreList.push(EneidList[n])
+        upDateScore();
+    } else {alert("Click Next Button"); }
+}, false);
+
+document.getElementById("Enedo").addEventListener('click', function () {
+    "use strict";
+    if (!isThisCellFull(idList[n]) && isTotalLessThanThree()) {
+        document.getElementById(EneidList[n]).innerHTML += 'D';
+        addedScoreList.push(EneidList[n])
+        upDateScore();
+    } else {alert("Click Next Button"); }
+}, false);
+
+document.getElementById("Enetu").addEventListener('click', function () {
+    "use strict";
+    if (!isThisCellFull(idList[n]) && isTotalLessThanThree()) {
+        document.getElementById(EneidList[n]).innerHTML += 'T';
+        addedScoreList.push(EneidList[n])
+        upDateScore();
+    } else {alert("Click Next Button"); }
+}, false);
+
+document.getElementById("Eneha").addEventListener('click', function () {
+    "use strict";
+    if (!isThisCellFull(EneidList[n]) && isTotalLessThanThree()) {
+        var FoundH = false;
+        var temp = document.getElementById(EneidList[n]).innerHTML;
+        for(var i = 0; i < document.getElementById(EneidList[n]).innerHTML.length; i++){
+//console.log(document.getElementById(EneidList[n]).innerHTML.substr(i,i+1));
+
+            if(document.getElementById(EneidList[n]).innerHTML.substr(i,i+1) === '▲'){
+                document.getElementById(EneidList[n]).innerHTML = temp.slice(0, i) + temp.slice(i + 1);
+//console.log("TEMP" + temp.slice(0, i) + temp.slice(i));
+                document.getElementById(EneidList[n]).innerHTML += 'H';
+                FoundH = true;
+            }
+        }
+        if(!FoundH){document.getElementById(EneidList[n]).innerHTML += '▲';}
+        addedScoreList.push(EneidList[n]);
+        upDateScore();
+    } else {alert("Click Next Button"); }
+}, false);
+
+document.getElementById('next').addEventListener('click', function () {
+    "use strict";
+    if(n < 4 && n > -1){ // means n is btw 0 to 3(one before last)
+      getWinnerNum();
+      upDateScore();
+      n++;
+      highLightCurrentColumn();
+      }
+    else { // n is more than 4 (last element)
+      getWinnerNum();
+      upDateScore();
+      writeWinner();
+      alert("Good Work! Match Finished");
+    }
+    console.log("In Next " + n);
+}, false);
+
+document.getElementById('undo').addEventListener('click', function() {
+    "use strict";
+    console.log("Before " + n);
+    undo();
+    getWinnerNum();
+    upDateScore();
+    console.log("after " + n);
+    highLightCurrentColumn();
+}, false);
